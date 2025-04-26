ff8ced0fefda906ced9058c6b4d07c75b15c2f3a
Andrew Krit <80667130+Andrew-Krit@users.noreply.github.com>
2025-04-26 03:42:46 +0300

    Added FileManager

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.List;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.print("Initial commit!");
+        var fileManager = new FileManager("data.txt");
+
+        var linesToWrite = List.of(
+                "String 1",
+                "String 2",
+                "String 3"
+        );
+
+        fileManager.writeToFile(linesToWrite, true);
+
+        fileManager.readFromFile()
+                .forEach(System.out::println);
     }
 }
